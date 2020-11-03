be75cef450f2561de4fac96c146545041f789eb2
Andrew Duffield <duffy518@gmail.com>
2020-11-03 10:52:26 -0800

    first pass on review writing

--- meals/review.py
+++ meals/review.py
@@ -38,25 +38,63 @@ def write(id):
     """Allows a user to write a review for a specific meal swipe"""
     # if the form is submitted, process it
     if request.method == "POST":
-        # todo: change everything within if
-        title = request.form["title"]
-        body = request.form["body"]
+        review_star = request.form.get("star")
+        review_text = request.form.get("review_text")
+
         error = None
 
-        if not title:
-            error = "Title is required."
+        if not review_star or not review_text:
+            error = "Both star and text and required"
 
         if error is not None:
-            flash(error)
+            return "ERROR: " + error
+
+        # return "thanks for reviewing " + str(review_star) + review_text
         else:
             db = get_db()
+            meal_swipe = db.execute(
+                f"""
+                    SELECT m.id, username, seller_id, buyer_id
+                    FROM meal_swipe m JOIN user u ON m.seller_id = u.id
+                    WHERE m.id={id}
+                    """
+            ).fetchone()
+            if not meal_swipe:
+                return "ERROR: Could not find meal swipe"
+
+            # todo: delete once buying is available
+            buyer_id = meal_swipe['buyer_id']
+            if not buyer_id:
+                buyer_id = 0
+
             db.execute(
-                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
+                "INSERT INTO review (seller_id, buyer_id, swipe_id, rating, description) VALUES (?, ?, ?, ?, ?)",
+                (meal_swipe['seller_id'], buyer_id, id, review_star, review_text),
             )
             db.commit()
-            # todo: add reviewee user id to redirect
-            return redirect(url_for("review.read"))
 
-    # if user is not submitting form, display page
-    return render_template("review/write.html")
+            return redirect(url_for("review.read", username=meal_swipe['username']))
+
+    # form is not submitted (GET request)
+    # get information on the review that is trying to be written
+    db = get_db()
+    meal_swipe = db.execute(
+        f"""
+        SELECT m.id, price, timestamp_buy, username, seller_id, buyer_id
+        FROM meal_swipe m JOIN user u ON m.seller_id = u.id
+        WHERE m.id={id}
+        """
+    ).fetchone()
+
+    error = None
+
+    if not meal_swipe:
+        error = 'Meal swipe not found'
+    # todo: reenable once buying is active
+    # elif meal_swipe['buyer_id'] != logged_in_user_id:
+    #     error = 'Only the buyer may write a review'
+
+    if error is not None:
+        return error
+    else:
+        return render_template("review/write.html", post=meal_swipe)
