0da79ad4e9b3e64aa890dc54dbb12be059877eab
Andrew Duffield <duffy518@gmail.com>
2020-12-03 09:59:56 -0800

    added tests, cleaned up code

--- tests/test_review.py
+++ tests/test_review.py
@@ -2,84 +2,43 @@ import pytest
 
 from meals.db import get_db
 
-# TODO: CHANGE ALL
 
-
-def test_index(client, auth):
+def test_read(client, auth):
     response = client.get("/")
-    assert b"Log In" in response.data
-    assert b"Register" in response.data
+    assert b"review" not in response.data
 
     auth.login()
+    response = client.get("/review/read/other")
+    assert b"test review" in response.data
+
+    response = client.get("/review/read/teqrwr")
+    assert b"Reviews for" in response.data
+
+    response = client.get("/review/read/test")
+    assert b"Reviews for" in response.data
+
+
+def test_write(app, client, auth):
     response = client.get("/")
-    assert b"test title" in response.data
-    assert b"by test on 2018-01-01" in response.data
-    assert b"test\nbody" in response.data
-    assert b'href="/1/update"' in response.data
+    assert b"review" not in response.data
 
+    auth.login()
+    response = client.get("/review/write/1")
+    assert b"You are reviewing swipe bought" in response.data
 
-@pytest.mark.parametrize("path", ("/create", "/1/update", "/1/delete"))
-def test_login_required(client, path):
-    response = client.post(path)
-    assert response.headers["Location"] == "http://localhost/auth/login"
+    response = client.get("/review/write/10")
+    assert b'Meal swipe not found' == response.data
 
+    response = client.post("/review/write/1", data={"review_star": "1", "review_text": ""})
+    assert b'ERROR: Both' in response.data
 
-def test_author_required(app, client, auth):
-    # change the post author to another user
+    response = client.post("/review/write/1", data={"review_star": "1", "review_text": "blah"})
+    assert response.status_code == 200
+
     with app.app_context():
         db = get_db()
-        db.execute("UPDATE post SET author_id = 2 WHERE id = 1")
+        db.execute("UPDATE meal_swipe SET buyer_id = 3 WHERE id = 1")
         db.commit()
 
-    auth.login()
-    # current user can't modify other user's post
-    assert client.post("/1/update").status_code == 403
-    assert client.post("/1/delete").status_code == 403
-    # current user doesn't see edit link
-    assert b'href="/1/update"' not in client.get("/").data
-
-
-@pytest.mark.parametrize("path", ("/2/update", "/2/delete"))
-def test_exists_required(client, auth, path):
-    auth.login()
-    assert client.post(path).status_code == 404
-
-
-def test_create(client, auth, app):
-    auth.login()
-    assert client.get("/create").status_code == 200
-    client.post("/create", data={"title": "created", "body": ""})
-
-    with app.app_context():
-        db = get_db()
-        count = db.execute("SELECT COUNT(id) FROM post").fetchone()[0]
-        assert count == 2
-
-
-def test_update(client, auth, app):
-    auth.login()
-    assert client.get("/1/update").status_code == 200
-    client.post("/1/update", data={"title": "updated", "body": ""})
-
-    with app.app_context():
-        db = get_db()
-        post = db.execute("SELECT * FROM post WHERE id = 1").fetchone()
-        assert post["title"] == "updated"
-
-
-@pytest.mark.parametrize("path", ("/create", "/1/update"))
-def test_create_update_validate(client, auth, path):
-    auth.login()
-    response = client.post(path, data={"title": "", "body": ""})
-    assert b"Title is required." in response.data
-
-
-def test_delete(client, auth, app):
-    auth.login()
-    response = client.post("/1/delete")
-    assert response.headers["Location"] == "http://localhost/"
-
-    with app.app_context():
-        db = get_db()
-        post = db.execute("SELECT * FROM post WHERE id = 1").fetchone()
-        assert post is None
+    response = client.get("/review/write/1")
+    assert b'Only the buyer may write a review' == response.data
